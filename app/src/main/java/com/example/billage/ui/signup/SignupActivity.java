3d1b8f44aaf8aab4404d11724af3e126af366143
Yeo IN Hyoek <cambria96@ajou.ac.kr>
2020-04-28 18:18:02 +0900

    2020-04-28-여인혁-메인페이지UI디자인-회원정보수정페이지 임시

--- app/src/main/java/com/example/billage/ui/signup/SignupActivity.java
+++ app/src/main/java/com/example/billage/ui/signup/SignupActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.billage.R;
 
@@ -17,5 +21,25 @@ public class SignupActivity extends AppCompatActivity {
         Intent intent = new Intent(this.getIntent());
         String tmp = intent.getStringExtra("tmp");
 
+
+        Button confirm = (Button) findViewById(R.id.confirm_btn);
+
+        // 입력완료 버튼 클릭 이벤트 처리
+        confirm.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+
+                // input 객체 선언
+                EditText input_text1 = (EditText) findViewById(R.id.name);
+                EditText input_text2 = (EditText) findViewById(R.id.nickname);
+
+                //객체에서 입력값 가져오기
+                String name = input_text1.getText().toString();
+                String nickname = input_text2.getText().toString();
+                Log.d("test",name);
+                Log.d("test",nickname);
+            }
+        });
     }
 }
