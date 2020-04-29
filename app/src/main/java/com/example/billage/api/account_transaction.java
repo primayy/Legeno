9936002553c2473ce1f63d5b60a41de5a3eac7f9
Yong Pyo Kim <primayy@ajou.ac.kr>
2020-04-29 20:42:48 +0900

    20200429-김용표-거래내역조회-거래내역조회 요청 부분 수정

--- app/src/main/java/com/example/billage/api/account_transaction.java
+++ app/src/main/java/com/example/billage/api/account_transaction.java
@@ -7,8 +7,11 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.example.billage.api.data.token.AccessToken;
 import com.example.billage.http.ApiCallAdapter;
 import com.google.gson.Gson;
+
+import org.jetbrains.annotations.NotNull;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -17,6 +20,7 @@ import retrofit2.Response;
 public class account_transaction {
     private HashMap<String, String> paramMap;
     private Bundle args;
+    private AccessToken accessToken;//앱데이터에 저장해서 발급받은 토큰 읽어오도록 설정해야함
 
     public EditText fromTime; //조회시작시간 20200428
     public EditText toTime; //조회종료시간 20200429
@@ -33,6 +37,29 @@ public class account_transaction {
         paramMap.put("sort_order", "D");//정렬순서
         paramMap.put("tran_dtime", "20200428101921");//요청일시 YYYYMMDDHHMMSS
         paramMap.put("befor_inquiry_trace_info", "20");//직전조회추적정보
+
+        //계좌번호 사용, 일단은 이것만 구현
+        paramMap.put("bank_code_std", "bankCode");
+        paramMap.put("account_num", "accountNum");
+        paramMap.put("user_seq_no", "userSeqNo");
+
+        //요청 부분
+        ApiCallAdapter.getInstance()
+                .accountTrasactionListAcntNum("Bearer" + accessToken, paramMap)
+                .enqueue(new Callback<Map>() {
+                    @Override
+                    public void onResponse(@NotNull Call<Map> call, @NotNull Response<Map> response) {
+                        if(response.isSuccessful()){
+                            Map result = response.body();
+                            System.out.println(result);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull Call<Map> call, @NotNull Throwable t) {
+                        System.out.println("통신실패: 서버 접속에 실패하였습니다.");
+                    }
+                });
 
 //        if(true){ // fintech 번호 사용시
 //            paramMap.put("fintech_use_num", getRandomBankTranId());//거래고유번호(참가은행)
