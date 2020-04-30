117cbccb75a3a7a3ba44abff4ca8688c7953565f
primayy <primayy@naver.com>
2020-04-30 22:03:55 +0900

    20200430-김용표-거래내역조회-거래내역조회 기능 1차 구현 완료, 추후 사용자 인증 기능 구현하면서 수정 필요

--- app/src/main/java/com/example/billage/api/data/token/TokenRequest.java
+++ app/src/main/java/com/example/billage/api/data/token/TokenRequest.java
@@ -1,10 +1,15 @@
 package com.example.billage.api.data.token;
 
+import android.content.SharedPreferences;
 import android.util.Log;
 
+import com.example.billage.common.AppData;
 import com.example.billage.http.ApiCallAdapter;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -17,13 +22,18 @@ import retrofit2.Response;
 
 public class TokenRequest {
     public static void request_token(){
-        String client_id = "5kCPktYvfvDDKT00EFPfbbaKgcK2ZzHgQl567QDb";
-        String client_secret = "tJFSvMaW1eybSx9l2ZG43c5ozfaAehWMJWFStMtl";
+        String client_id = "dukjFhxkn3ji94NDLBzGCZwMomK526dfK9M6cIM8";
+        String client_secret = "2zsTRQkDSavQP170dpTZmvbnDzEZNT61NFCTEnT7";
+        String auth_code = "m0WMfi2oSHfBk0SyDrfMAupzsoGiCD"; //user별 코드 받도록 해야함
         HashMap<String, String> paramMap = new HashMap<>();
+
+        paramMap.put("code", auth_code);
         paramMap.put("client_id", client_id);
         paramMap.put("client_secret", client_secret);
-        paramMap.put("scope", "oob");   // 고정값
-        paramMap.put("grant_type", "client_credentials");   // 고정값
+//        paramMap.put("scope", "oob");   // 고정값
+//        paramMap.put("grant_type", "client_credentials");   // 고정값
+        paramMap.put("grant_type", "authorization_code");   // 고정값
+        paramMap.put("redirect_uri", "https://localhost:8890/callback");   // 고정값
 
         ApiCallAdapter.getInstance()
                 .token(paramMap)
@@ -33,6 +43,24 @@ public class TokenRequest {
                         if(response.isSuccessful()){
                             String responseJson = new Gson().toJson(response.body());
                             Log.d("token",responseJson);
+
+                            //json 파싱
+                            JsonObject json = new JsonParser().parse(responseJson).getAsJsonObject();
+                            JsonElement token = json.get("access_token");
+                            String value = token.getAsString();
+                            Log.d("token",value);
+
+                            JsonElement client_use_code = json.get("client_use_code");
+                            String cl_use_code = client_use_code.getAsString();
+
+                            //앱데이터에 저장
+                            SharedPreferences.Editor editor = AppData.getPref().edit();
+                            editor.putString("access_token",value);
+                            editor.putString("client_use_code",cl_use_code);
+
+                            editor.apply();
+
+
 
                         }
                     }
