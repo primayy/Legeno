1d30d7fd2306363fada25cfe9120fedf3414a2f1
doseum <chswoo7@ajou.ac.kr>
2020-05-07 20:36:04 +0900

    20200507-정승우-android2nodejs post class구현 및 회원정보 post 구현

--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app= express()
 var router = express.Router();
-var bodyParser=require('body-Parser')
+var bodyParser=require('body-parser')
 
 app.use(bodyParser.urlencoded({extended:true}))
 
@@ -22,21 +22,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/SignUp',function(req,res){
-  if(req.body.name){//이름 검사
-    if(req.body.nickname){//닉네임 검사
+router.post('/SignUp',function(req,res){
+  //데이터를 json형식으로 parsing
+  var str=JSON.stringify(req.body);
+  var postdata=JSON.parse(JSON.parse(str.substring(1,str.length-4)));
+  console.log(postdata);
+
+  if(postdata.name){//이름 검사
+    if(postdata.nickname){//닉네임 검사
       connection.query("select nickname from Billage.user",function(err,rows,fields){
         if(!err) {
           var nicknameList=rows
           console.log(nicknameList);
           for (var dbNick of nicknameList){
-            if(req.body.nickname==dbNick.nickname){//닉네임 중복검사
+            if(postdata.nickname==dbNick.nickname){//닉네임 중복검사
               console.log('occupied nickname!');
               return
             }
           }
           //if(req.body.password) 비밀번호 유효성검사
-          connection.query(`insert into Billage.user(user_name,nickname,bank_list,auth_check) values(${req.body.name},${req.body.nickname},NULL,false)`)
+          connection.query(`insert into Billage.user(user_name,nickname,bank_list,auth_check) values("${postdata.name}","${postdata.nickname}",NULL,false)`)
         }
         else{
           console.log('nickname select query error');
