473ec56ba91e9759ec24dc5cc4387cf8d0068d7e
doseum <chswoo7@ajou.ac.kr>
2020-05-10 15:02:57 +0900

    202005010-정승우-Game파트 getUserDB,addObjinfo 추가

--- Backend/app.js
+++ Backend/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var gameRouter = require('./routes/game.js');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/UserBack', indexRouter);
+app.use('/Game', gameRouter);
 //app.use('/users', usersRouter);
 
 module.exports = app;
