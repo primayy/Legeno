1b6028a7ff2c281f75e1072360c033503b2576a0
doseum <chswoo7@ajou.ac.kr>
2020-05-22 22:52:39 +0900

    20200522-정승우-DB업데이트 및 reader추가

---
+++ Backend/routes/DBReader.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app= express()
+var router = express.Router();
+var mysql=require('mysql')
+var bodyParser=require('body-parser')
+
+app.use(bodyParser.urlencoded({extended:true}))
+
+var connection=mysql.createConnection({
+  host:'3.17.141.131',
+  port:3306,
+  user:'root',
+  password:'*tmddn1010914',
+  database:'Billage'
+})
+
+connection.connect();
+
+module.exports = router;
