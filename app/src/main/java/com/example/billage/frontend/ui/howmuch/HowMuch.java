2d96f9dfd80e8ba049c4f39498372169b125d522
Yeo IN Hyoek <cambria96@ajou.ac.kr>
2020-06-04 21:27:31 +0900

    20200604-여인혁- 얼마 썼는지 궁금햬? UI 구현 - seekbar 구현

--- app/src/main/java/com/example/billage/frontend/ui/howmuch/HowMuch.java
+++ app/src/main/java/com/example/billage/frontend/ui/howmuch/HowMuch.java
@@ -3,7 +3,12 @@ package com.example.billage.frontend.ui.howmuch;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
 import com.example.billage.R;
+import com.warkiz.widget.IndicatorSeekBar;
+import com.warkiz.widget.OnSeekChangeListener;
+import com.warkiz.widget.SeekParams;
 
 public class HowMuch extends AppCompatActivity {
 
@@ -11,5 +16,31 @@ public class HowMuch extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_much);
+
+        IndicatorSeekBar seekBar = findViewById(R.id.seek_bar);
+
+
+        TextView header = findViewById(R.id.how_much_header);
+
+        seekBar.setIndicatorTextFormat("${TICK_TEXT}개월");
+
+
+        seekBar.setOnSeekChangeListener(new OnSeekChangeListener() {
+            @Override
+            public void onSeeking(SeekParams seekParams) {
+                header.setText(seekBar.getProgress()+"개월간 총 지출은 200000원 입니다.");
+            }
+
+            @Override
+            public void onStartTrackingTouch(IndicatorSeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(IndicatorSeekBar seekBar) {
+
+            }
+        });
+
     }
 }
