f704c1fb874575eb7f112fdcb84d211fc6665a47
Yeo IN Hyoek <cambria96@ajou.ac.kr>
2020-06-04 11:55:13 +0900

    20200604-여인혁- 얼마 썼는지 궁금햬? UI 구현 - floating button 수정

---
+++ app/src/main/java/com/example/billage/frontend/ui/howmuch/HowMuch.java
@@ -0,0 +1,15 @@
+package com.example.billage.frontend.ui.howmuch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.billage.R;
+
+public class HowMuch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_much);
+    }
+}
